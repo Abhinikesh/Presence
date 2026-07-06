c7deeeb58113d2cc6370b6610b297f4173152103
Abhinikesh <abhinikeshkumar1@gmail.com>
2026-07-06 22:34:23 +0530

    aouth fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.jsx'
 
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || 'your_google_client_id.apps.googleusercontent.com';
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '766065422203-kn5v6n1cem8ah8efjstokcebbs1kg1r8.apps.googleusercontent.com';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
